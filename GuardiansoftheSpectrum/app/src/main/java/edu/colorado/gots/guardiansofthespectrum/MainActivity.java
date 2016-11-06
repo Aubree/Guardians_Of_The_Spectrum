d4b95e3f8f3aa7411724cfd77accc65707e30b6b
stranchetti <sean.tranchetti@colorado.edu>
2016-11-05 19:15:22 -0600

    linked in wifi code to main screen

--- GuardiansoftheSpectrum/app/src/main/java/edu/colorado/gots/guardiansofthespectrum/MainActivity.java
+++ GuardiansoftheSpectrum/app/src/main/java/edu/colorado/gots/guardiansofthespectrum/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.colorado.gots.guardiansofthespectrum;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +19,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mTextView = (TextView)findViewById(R.id.textview1);
         mTextView.setText(R.string.Hello);
+    }
+
+    //start the scan activity after we get a button click
+    public void initiateScan(View v) {
+        Intent i = new Intent(this, WifiScanActivity.class);
+        startActivity(i);
     }
 }
