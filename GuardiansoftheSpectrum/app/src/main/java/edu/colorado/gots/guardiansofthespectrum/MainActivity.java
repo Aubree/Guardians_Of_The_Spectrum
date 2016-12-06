9bcf623efee274a7db3920e0b6a14f2d5d78694f
stranchetti <sean.tranchetti@colorado.edu>
2016-12-06 16:57:48 -0700

    integrated service into basic skeleton

--- GuardiansoftheSpectrum/app/src/main/java/edu/colorado/gots/guardiansofthespectrum/MainActivity.java
+++ GuardiansoftheSpectrum/app/src/main/java/edu/colorado/gots/guardiansofthespectrum/MainActivity.java
@@ -4,10 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView mTextView;
+    Switch serviceSwitch;
+    Intent serviceIntent;
 
     /**
      *
@@ -19,11 +23,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mTextView = (TextView)findViewById(R.id.textview1);
         mTextView.setText(R.string.Hello);
+        serviceSwitch = (Switch) findViewById(R.id.scanServiceSwitch);
+        serviceIntent = new Intent(this, ScanService.class);
+        serviceSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton button, boolean isChecked) {
+                if (isChecked) {
+                    //service is enabled so start it up!
+                    startService(serviceIntent);
+                } else {
+                    stopService(serviceIntent);
+                }
+            }
+        });
     }
 
     //start the scan activity after we get a button click
     public void initiateScan(View v) {
-        Intent i = new Intent(this, WifiScanActivity.class);
+        Intent i = new Intent(this, ScanActivity.class);
         startActivity(i);
     }
 }
--- GuardiansoftheSpectrum/app/src/main/java/edu/colorado/gots/guardiansofthespectrum/ScanService.java
+++ GuardiansoftheSpectrum/app/src/main/java/edu/colorado/gots/guardiansofthespectrum/ScanService.java
@@ -2,23 +2,32 @@ package edu.colorado.gots.guardiansofthespectrum;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.content.Context;
 
 public class ScanService extends IntentService {
+
+    private boolean running = true;
 
     public ScanService() {
         //provides a name for our service thread
         super("ScanService");
     }
 
+    //this will be called when the stopService() command is run from the main home screen
+    public void onDestroy() {
+        //tell our scan that it needs to stop
+        running = false;
+        super.onDestroy();
+    }
+
     //the main meat of our code can go in here. Once this function returns
     //our service will exit itself
     protected void onHandleIntent(Intent intent) {
-        while (true) {
+        while (running) {
             System.out.println("Running background Scan Service\n");
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {}
         }
     }
+
 }
