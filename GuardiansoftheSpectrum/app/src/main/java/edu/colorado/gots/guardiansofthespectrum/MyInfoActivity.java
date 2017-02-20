cccb48ed0d213c4fa7a6f52b52eb0532ae53b05b
jubarnitz <justin.barnitz@colorado.edu>
2017-02-19 17:56:49 -0700

    Revert "working with mpandroid chart"
    This reverts commit 90c93f1e8edb33ea909a8576f28b0f9bbec7b3e6.

--- GuardiansoftheSpectrum/app/src/main/java/edu/colorado/gots/guardiansofthespectrum/MyInfoActivity.java
+++ GuardiansoftheSpectrum/app/src/main/java/edu/colorado/gots/guardiansofthespectrum/MyInfoActivity.java
@@ -1,34 +1,12 @@
 package edu.colorado.gots.guardiansofthespectrum;
 
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
-import android.widget.TextView;
 
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.data.Entry;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class MyInfoActivity extends BaseActivity {
-    TextView mTextView;
-    String [] LTEdata;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myinfo);
-
-        // in this example, a LineChart is initialized from xml
-        LineChart chart = (LineChart) findViewById(R.id.chart);
-
-        mTextView = (TextView)findViewById(R.id.textView4);
-        mTextView.setMovementMethod(new ScrollingMovementMethod());
-
-        LTEdata = new DataFileManager(getApplicationContext()).readAllDataFiles();
-
-        mTextView.setText(Arrays.toString(LTEdata));
-        //mTextView.setText(LTEdata[0]);
     }
 }
