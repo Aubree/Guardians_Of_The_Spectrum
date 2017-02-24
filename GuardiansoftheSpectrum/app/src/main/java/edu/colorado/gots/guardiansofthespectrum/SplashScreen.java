3adde91398e47ed5de0e70aface026a7f9fc4aa1
Aubree <aubree@lytwyn.com>
2017-02-24 11:42:28 -0700

    Finished splash screen

---
+++ GuardiansoftheSpectrum/app/src/main/java/edu/colorado/gots/guardiansofthespectrum/SplashScreen.java
@@ -0,0 +1,33 @@
+package edu.colorado.gots.guardiansofthespectrum;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.widget.Toolbar;
+
+public class SplashScreen extends BaseActivity {
+
+    //Timer for how long splash screen lasts
+    private static int SPLASH_TIME_OUT = 2000;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            /*Once the timer is done the run method will be executed and the main activity will load up*/
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
