298b20ef25cfbee0071894e3e59b02b355a92a99
Aubree <aubree@lytwyn.com>
2017-03-21 22:40:42 -0700

    Imported powerBi map in main screen

--- GuardiansoftheSpectrum/app/src/main/java/edu/colorado/gots/guardiansofthespectrum/MainActivity.java
+++ GuardiansoftheSpectrum/app/src/main/java/edu/colorado/gots/guardiansofthespectrum/MainActivity.java
@@ -3,6 +3,8 @@ package edu.colorado.gots.guardiansofthespectrum;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.Button;
 
 
@@ -16,6 +18,13 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Create visualization using PowerBI
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("https://app.powerbi.com/view?r=eyJrIjoiMjYxMTNiMzQtYjNlNi00ZGM1LTk1MWMtYmRhMzQ2OWZhNGIxIiwidCI6IjNkZWQ4YjFiLTA3MGQtNDYyOS04MmU0LWMwYjAxOWY0NjA1NyIsImMiOjZ9");
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
 
         Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
